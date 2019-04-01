5421119be22d756aee087591375cb62008cb949f
Wonbo Lee <aplus127@sogang.ac.kr>
2019-04-01 15:06:51 +0900

    checklist

---
+++ Checklist/Checklist/ChecklistViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Checklist
+//
+//  Created by 이원보 on 01/04/2019.
+//  Copyright © 2019 이원보. All rights reserved.
+//
+
+import UIKit
+
+class ChecklistViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
